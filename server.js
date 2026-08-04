0da113335a0c99ee3e6d787458cd521e74daae38
PhuDepTrai137 <plthienphu2012@gmail.com>
2026-08-04 12:20:44 +0700

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("TPTool API is running!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
